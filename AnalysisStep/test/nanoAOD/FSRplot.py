8430130423b4cb6d4487cc888fc2cf3c450c1939
Valentina <valentina.picco@cern.ch>
2021-05-25 13:23:56 +0200

    FSRplots updated

--- AnalysisStep/test/nanoAOD/FSRplot.py
+++ AnalysisStep/test/nanoAOD/FSRplot.py
@@ -1,55 +1,50 @@
+
 # Plot files produced with FSRtest.py
 #
 from __future__ import print_function
 import math
-from ROOT import TCanvas, TFile, TColor, gStyle
-from ROOT import kBlack, kBlue, kRed, kOrange, kGreen
+from ROOT import TAttLine,TCanvas, TFile, TColor, gStyle, TH1F, TH1, gROOT
+from ROOT import kBlack, kBlue, kRed, kOrange, kGreen, kPink,kCyan
+
+def printCanvases(type="png", path=".") :
+    canvases = gROOT.GetListOfCanvases()
+    for c in canvases :
+        c.Print(path+"/"+c.GetTitle()+"."+type)
 
 ifile=TFile("fsrHisto.root")
 
 gStyle.SetOptTitle(0)
 gStyle.SetOptStat(0)
 gStyle.SetHistLineWidth(4)
-
-c1 = TCanvas( 'FSRLepPts', 'FSRLepPts',)
-ifile.fsrMuPt.SetLineColor(kRed)
-ifile.fsrElePt.SetLineColor(kBlue)
-ifile.fsrMuPt.Draw()
-ifile.fsrElePt.Draw("same")
-
-c2 = TCanvas( 'FSRMass_4e', '4e Candidates with FSR',)
-ifile.ZZMassFSR_4e.SetLineColor(kBlack)
-ifile.ZZMassNoFSR4e.SetLineColor(kOrange)
-ifile.ZZMassFSRTrue4e.SetLineColor(kRed)
-ifile.ZZMassFSRFake4e.SetLineColor(kGreen)
-ifile.ZZMassFSR_4e.SetLineWidth(2)
-ifile.ZZMassNoFSR4e.SetLineWidth(2)
-ifile.ZZMassFSRTrue4e.SetLineWidth(2)
-ifile.ZZMassFSRFake4e.SetLineWidth(2)
-ifile.ZZMassFSR_4e.Draw()
-ifile.ZZMassNoFSR4e.Draw("same")
-ifile.ZZMassFSRTrue4e.Draw("same")
-ifile.ZZMassFSRFake4e.Draw("same")
-ifile.ZZMassFSR_4e.Draw("same") #redraw on top
-
-c3 = TCanvas( 'FSRMass_4mu', '4mu Candidates with FSR',)
-ifile.ZZMassFSR_4mu.SetLineColor(kBlack)
-ifile.ZZMassNoFSR4mu.SetLineColor(kOrange)
-ifile.ZZMassFSRTrue4mu.SetLineColor(kRed)
-ifile.ZZMassFSRFake4mu.SetLineColor(kGreen)
-ifile.ZZMassFSR_4mu.SetLineWidth(2)
-ifile.ZZMassNoFSR4mu.SetLineWidth(2)
-ifile.ZZMassFSRTrue4mu.SetLineWidth(2)
-ifile.ZZMassFSRFake4mu.SetLineWidth(2)
-ifile.ZZMassFSR_4mu.Draw()
-ifile.ZZMassNoFSR4mu.Draw("same")
-ifile.ZZMassFSRTrue4mu.Draw("same")
-ifile.ZZMassFSRFake4mu.Draw("same")
-ifile.ZZMassFSR_4mu.Draw("same") #redraw on top
+gStyle.SetPadGridX(True);
+gStyle.SetPadGridY(True);
+gStyle.SetGridColor(0);
+gStyle.SetGridStyle(3);
+gStyle.SetGridWidth(1);
 
 
-def plotTrueFake(hFSR,hNoFSR,hTrue,hFalse) :
+TH1.SetDefaultSumw2()
+
+def plotLepPt(hMu,hEle,hMuFake,hEleFake):
+    hMu.SetLineColor(kRed)
+    hMu.GetXaxis().SetTitle("p_{t} [GeV/c^{2}]")
+    hMu.GetYaxis().SetTitle("Arbitrary Units")
+    hEle.SetLineColor(kBlue)
+    hMuFake.SetLineColor(kPink-9)
+    hEleFake.SetLineColor(kCyan-6)
+    hMu.Draw()
+    hEle.Draw("same")
+    hMuFake.Draw("same")
+    hEleFake.Draw("same")
+
+c1 = TCanvas( 'FSRLepPts', 'FSRLepPts',800,800)
+plotLepPt(ifile.fsrMuPt, ifile.fsrElePt, ifile.fsrMuPtFake, ifile.fsrElePtFake)
+
+
+def plot(hFSR,hNoFSR,hTrue,hFalse) :
     hFSR.SetLineColor(kBlack)
+    hFSR.GetXaxis().SetTitle("m_{4 #it{l} } [GeV/c^{2}]")
+    hFSR.GetYaxis().SetTitle("Arbitrary Units")    
     hNoFSR.SetLineColor(kOrange)
     hTrue.SetLineColor(kRed)
     hFalse.SetLineColor(kGreen)
@@ -63,9 +58,29 @@ def plotTrueFake(hFSR,hNoFSR,hTrue,hFalse) :
     hFalse.Draw("same")
     hFSR.Draw("same") #redraw on top
     
-c4 = TCanvas("FSRMass_2e2mu","FSRMass_2e2mu")
-plotTrueFake(ifile.ZZMassFSR_2e2mu,ifile.ZZMassNoFSR2e2mu,ifile.ZZMassFSRTrue2e2mu,ifile.ZZMassFSRFake2e2mu)
+c2 = TCanvas( 'CandMass_4e_FSR', 'CandMass_4e_FSR',800,800)
+plot(ifile.ZZMassFSR_4e, ifile.ZZMassNoFSR4e, ifile.ZZMassFSRTrue4e, ifile.ZZMassFSRFake4e)
+
+c3 = TCanvas( 'CandMass_4mu_FSR', 'CandMass_4mu_FSR',800,800)
+plot(ifile.ZZMassFSR_4mu, ifile.ZZMassNoFSR4mu, ifile.ZZMassFSRTrue4mu, ifile.ZZMassFSRFake4mu)
+
+c4 = TCanvas("CandMass_2e2mu_FSR","CandMass_2e2mu_FSR",800,800)
+plot(ifile.ZZMassFSR_2e2mu,ifile.ZZMassNoFSR2e2mu,ifile.ZZMassFSRTrue2e2mu,ifile.ZZMassFSRFake2e2mu)
+
 
 
 purity = 1.-ifile.fsrET2vsDR_fake.GetEntries()/(ifile.fsrET2vsDR_fake.GetEntries()+ifile.fsrET2vsDR_true.GetEntries())
 print("FSR purity: = {:.3g}".format(purity))
+
+
+h_purityMu = TH1F("FSR purity muon","FSR purity muon",200,0.,200.)
+c5=TCanvas ("FSRPurity_muon","FSRPurity_muon",800,800)
+h_purityMu.Divide(ifile.fsrMuPtTrue,ifile.fsrMuPt,1.,1.,"b")
+h_purityMu.SetLineWidth(1)
+h_purityMu.Draw("PE")
+
+h_purityEle = TH1F("FSR purity ele","FSR purity ele",200,0.,200.)
+c6=TCanvas ("FSRPurity_ele","FSRPurity_ele",800,800)
+h_purityEle.Divide(ifile.fsrElePtTrue,ifile.fsrElePt,1.,1.,"b")
+h_purityEle.SetLineWidth(1)
+h_purityEle.Draw("PE")
